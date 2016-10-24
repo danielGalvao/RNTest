cd8b759f9000ca259e147450293aba76f1301ad4
Daniel Galvão <galvaodev@gmail.com>
2016-10-23 22:24:13 -0200

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+export default class RNtest extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+       style={{width: 50, height: 50}} />
+        <Text style={styles.welcome}>
+          AE PROJETINHO!!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('RNtest', () => RNtest);
